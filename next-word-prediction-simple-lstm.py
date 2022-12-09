00d6a4d87250e8121a1c1b7159f5d059d8d23f9d
Shivam Pande <shivampande3@gmail.com>
2022-12-09 01:18:08 -0500

    First commit

---
+++ next-word-prediction-simple-lstm.py
@@ -0,0 +1,179 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import os
+import numpy as np
+
+import tensorflow as tf
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.layers import Embedding, LSTM, Dense, Bidirectional
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
+
+
+# In[2]:
+
+
+medium_data = pd.read_csv('./medium_data.csv')
+medium_data.head()
+
+
+# In[3]:
+
+
+print("Number of records: ", medium_data.shape[0])
+print("Number of fields: ", medium_data.shape[1])
+
+
+# In[4]:
+
+
+medium_data['title']
+
+
+# In[5]:
+
+
+medium_data['title'] = medium_data['title'].apply(lambda x: x.replace(u'\xa0',u' '))
+medium_data['title'] = medium_data['title'].apply(lambda x: x.replace('\u200a',' '))
+
+
+# In[6]:
+
+
+tokenizer = Tokenizer(oov_token='<oov>') # For those words which are not found in word_index
+tokenizer.fit_on_texts(medium_data['title'])
+total_words = len(tokenizer.word_index) + 1
+
+print("Total number of words: ", total_words)
+print("Word: ID")
+print("------------")
+print("<oov>: ", tokenizer.word_index['<oov>'])
+print("Strong: ", tokenizer.word_index['strong'])
+print("And: ", tokenizer.word_index['and'])
+print("Consumption: ", tokenizer.word_index['consumption'])
+
+
+# In[7]:
+
+
+input_sequences = []
+for line in medium_data['title']:
+    token_list = tokenizer.texts_to_sequences([line])[0]
+    #print(token_list)
+    
+    for i in range(1, len(token_list)):
+        n_gram_sequence = token_list[:i+1]
+        input_sequences.append(n_gram_sequence)
+
+# print(input_sequences)
+print("Total input sequences: ", len(input_sequences))
+
+
+# In[8]:
+
+
+# pad sequences 
+max_sequence_len = max([len(x) for x in input_sequences])
+input_sequences = np.array(pad_sequences(input_sequences, maxlen=max_sequence_len, padding='pre'))
+input_sequences[1]
+
+
+# In[9]:
+
+
+# create features and label
+xs, labels = input_sequences[:,:-1],input_sequences[:,-1]
+ys = tf.keras.utils.to_categorical(labels, num_classes=total_words)
+
+
+# In[10]:
+
+
+print(xs[5])
+print(labels[5])
+print(ys[5][14])
+
+
+# In[11]:
+
+
+model = Sequential()
+model.add(Embedding(total_words, 100, input_length=max_sequence_len-1))
+model.add(LSTM(150))
+model.add(Dense(total_words, activation='softmax'))
+adam = Adam(lr=0.01)
+model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
+history = model.fit(xs, ys, epochs=50, verbose=1)
+#print model.summary()
+print(model)
+
+
+# In[12]:
+
+
+import matplotlib.pyplot as plt
+
+
+def plot_graphs(history, string):
+    plt.plot(history.history[string])
+    plt.xlabel("Epochs")
+    plt.ylabel(string)
+    plt.show()
+
+
+# In[13]:
+
+
+plot_graphs(history, 'accuracy')
+
+
+# In[14]:
+
+
+plot_graphs(history, 'loss')
+
+
+# In[15]:
+
+
+seed_text = "Implementation of rnn"
+next_words = 2
+  
+for _ in range(next_words):
+    token_list = tokenizer.texts_to_sequences([seed_text])[0]
+    token_list = pad_sequences([token_list], maxlen=max_sequence_len-1, padding='pre')
+#     predicted = model.predict_classes(token_list, verbose=0)
+    predict_x=model.predict(token_list, verbose=0) 
+    classes_x=np.argmax(predict_x,axis=1)
+    output_word = ""
+    for word, index in tokenizer.word_index.items():
+        if index == classes_x:
+            output_word = word
+            break
+    seed_text += " " + output_word
+print(seed_text)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
